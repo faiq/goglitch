a1633309e07634e4bc7ebb731bebd6e89b68e498
faiq <faiqrazarizvi@gmail.com>
2015-11-12 23:05:51 -0500

    some sort of random algorithim to do a vertical sort

--- glitcher/vertical-sort.go
+++ glitcher/vertical-sort.go
@@ -7,6 +7,9 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+	"math"
+	"math/big"
+	"math/rand"
 	"os"
 	"sort"
 )
@@ -23,7 +26,18 @@ func VerticalSort(input string, outfile string) error {
 	}
 	bounds := img.Bounds()
 	sortedImg := image.NewNRGBA(bounds)
+	sortedRegionSize := math.Floor(float64(bounds.Max.Y) * .05) // sort 5 % of the picture at any given time
+	var numSortedCols float64
+	randSrc := rand.NewSource(1000000000)
+	RNJesus := rand.New(randSrc)
+	shouldSort := false
 	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		if numSortedCols == 0 {
+			// generate a random number, see if it passes a test
+			if n := big.NewInt(int64(RNJesus.Int())); n.ProbablyPrime(2) {
+				shouldSort = true
+			}
+		}
 		var vals []int
 		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			r, g, b, _ := img.At(x, y).RGBA()
@@ -33,7 +47,15 @@ func VerticalSort(input string, outfile string) error {
 			val := uint32(0 | uint32(r)<<8 | uint32(g)<<16 | uint32(b)<<24)
 			vals = append(vals, int(val))
 		}
-		sort.Ints(vals)
+		if shouldSort {
+			sort.Ints(vals)
+			if numSortedCols < sortedRegionSize {
+				numSortedCols++
+			} else {
+				numSortedCols = 0
+				shouldSort = false
+			}
+		}
 		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			r := vals[y] >> 8
 			g := vals[y] >> 16
